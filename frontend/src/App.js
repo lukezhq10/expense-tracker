62c159be119b48fa4c44e499836f93185d302289
Luke Zhang <you@example.com>
2023-09-30 23:46:25 -0600

    added navbar styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,30 @@ import bg from './img/bg.png';
 import {MainLayout} from './styles/layouts';
 import Orb from './Components/Orb/Orb';
 import Navigation from './Components/Navigation/Navigation';
+import { useMemo, useState } from 'react';
 
 function App() {
+  const [active, setActive] = useState(1);
+
+  const displayData = () => {
+    switch(active) {
+      case 1:
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
   return (
     <AppStyled bg={bg} className='App'>
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+        <main>
+          {displayData()}
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -19,6 +36,18 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
   position: relatve;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow: auto;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 export default App;
---
+++ frontend/src/Components/Expenses/Expenses.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Expenses() {
+    return (
+        <div>Expenses</div>
+    )
+}
+
+const ExpensesStyled = styled.div`
+
+`;
+
+export default Expenses
